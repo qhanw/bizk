69c2dbb7ed3e6f02d82aed5790b2e6342acc56e9
qhan <whenhan@foxmail.com>
2022-12-20 10:36:27 +0800

    feat(@libra/biz): toolkit integration

--- packages/biz/src/index.ts
+++ packages/biz/src/index.ts
@@ -1,1 +1,2 @@
+export { renameKeys } from '@libra/utils';
 export { default as calcTableOptWidth } from './calcTableOptWidth';
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,1 @@
-export { default as calcTableOptWidth } from './calcTableOptWidth';
-
 export { default as renameKeys } from './renameKeys';
