1120ac40ebcd1c5853b0c5160a33e86520e481f3
qhan <whenhan@foxmail.com>
2023-03-28 14:14:37 +0800

    feat: set table width when dragging

--- packages/hooks/src/thead-stretch-next/components/useDragCell.ts
+++ packages/hooks/src/thead-stretch-next/components/useDragCell.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useLayoutEffect } from 'react';
 
 import { parents, cssToObj, objToCss, limit } from '../utils';
 
@@ -23,7 +23,7 @@ export default function useDragCell({
 }: DragCell) {
   const refOffset = useRef<number>(0);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!th) return;
 
     const handle = th.querySelector(
@@ -51,44 +51,50 @@ export default function useDragCell({
       // 计算游标偏移量
       const x = Math.round(rect.x - tRect.x + offset - 2);
 
-      t.parentNode.parentNode.style.cssText = `--dnd-translate-x: ${x}px`;
+      // t.parentNode.parentNode.style.cssText = `--dnd-translate-x: ${x}px`;
+      t.style.cssText = objToCss({
+        ...cssToObj(t.style.cssText),
+        '--dnd-translate-x': `${x}px`,
+      });
 
       refOffset.current = offset; // 存储最后偏移位置,以便后续设置对应单元格宽度
     } else {
       // 停止拖动时，设置单元格最终宽度
-      t.parentNode.parentNode.removeAttribute('style');
+      // t.parentNode.parentNode.removeAttribute('style');
+
+      // 游标样式清理
+      const cssObj = cssToObj(t.style.cssText);
+      delete cssObj['--dnd-translate-x'];
+      t.style.cssText = objToCss(cssObj);
 
       // 当发生偏移时设置当前单元格宽度
       if (refOffset.current !== 0) {
-        const cr = t.querySelector(':scope colgroup');
-        // 计算 当前单元格宽度
-        const thRect = th.getBoundingClientRect();
+        const r = t.querySelector(':scope colgroup');
+        // 计算 当前单元格宽度与表格宽度，需在设置宽度前获取初始元素宽度信息
+        const tht = th.getBoundingClientRect(); // 表头单元格
+        const trt = t.getBoundingClientRect(); //  表格
 
         const mark = `--dnd-col-${th.getAttribute('id')}`;
 
-        const oldCss = cssToObj(cr.style.cssText);
+        const oldCssR = cssToObj(r.style.cssText);
+        const oldCssT = cssToObj(t.style.cssText);
 
-        const { width: w, offset: realOffset } = limit(thRect.width, {
+        // 单元格宽度与真实偏移量
+        const { width: w, offset: real } = limit(tht.width, {
           min: minLimit,
           max: maxLimit,
           offset: refOffset.current,
         });
 
-        cr.style.cssText = objToCss({ ...oldCss, [mark]: `${w}px` });
+        // 表格宽度
+        const tWidth = trt.width + real;
 
-        // 当偏移量为正数时，表格宽度为当前宽度加上偏移量
-        // 当偏移量为负数时，表格宽度恢复到100%，并
+        // 设置单元格宽度
+        r.style.cssText = objToCss({ ...oldCssR, [mark]: `${w}px` });
 
         // 设置表格宽度
-        const tRect = t.getBoundingClientRect();
-        // console.log(tRect);
-
-        // console.log(w, realOffset, refOffset.current, tRect.width);
-
-        t.style.cssText = objToCss({
-          ...cssToObj(t.style.cssText),
-          width: `${tRect.width + realOffset}px`,
-        });
+        // 当偏移量为正数时，表格宽度为当前宽度加上偏移量，当偏移量为负数时，表格宽度恢复到100%，并
+        t.style.cssText = objToCss({ ...oldCssT, width: `${tWidth}px` });
       }
     }
   }, [isDragging, offset, th]);
